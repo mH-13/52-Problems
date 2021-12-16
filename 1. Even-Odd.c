0ad399ec1759fbbd6a37cabe465f78a3c11a25fd
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-12-16 14:29:59 +0600

    Update 1. Even-Odd.c

--- 1. Even-Odd.c
+++ 1. Even-Odd.c
@@ -2,7 +2,7 @@
 int main(){
     int T, N;
     scanf("%d", &T);
-    for(int i=0; i<=T; i++){
+    for(int i=1; i<=T; i++){
         scanf("%d", &N);
         if(N%2 == 0){
             printf("Even\n");
