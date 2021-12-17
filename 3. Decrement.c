4f6b7cdbd4e7dc97d28bbdc3bea1898f1c118d39
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-12-17 15:31:34 +0600

    Update 3. Decrement.c

--- 3. Decrement.c
+++ 3. Decrement.c
@@ -6,3 +6,18 @@ int main(){
     }
     return 0;
 }
+
+//Or
+
+#include <stdio.h>
+int main(){
+    for(int i=1000; i>=1; i--){
+        if(i%5==0){
+            printf("\n");
+        }
+        printf("%d\t", i);
+
+    }
+    printf("\n");
+    return 0;
+}
