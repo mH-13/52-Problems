65d95f27951e1b0f30c509c52f22213bb167996e
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-12-17 20:09:30 +0600

    Update 4. Factor.c

--- 4. Factor.c
+++ 4. Factor.c
@@ -4,7 +4,7 @@ int main(){
     scanf("%d", &n);
     for(i=1; i<=n; i++){
         scanf("%d", &num);
-        printf("Case 1:\t");
+        printf("Case %d:\t", i);
         for(int j=1;j<=num; j++){
             if(num%j==0){
                 printf("%d ", j);
