07270b1ce37907ed96c529f9bffbda96b87213d3
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-12-17 12:38:40 +0600

    Update 3. Decrement.c

--- 3. Decrement.c
+++ 3. Decrement.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 int main(){
     for(int i=1000; i >=1; i = i-5){
-        printf("%d  ", i);
-        printf("%d  ", i-1);
-        printf("%d  ", i-2);
-        printf("%d  ", i-3);
-        printf("%d  \n", i-4);
+        printf("%d  %d  %d  %d  %d\n", i, i-1,i-2,i-3,i-4);
 
     }
     return 0;
