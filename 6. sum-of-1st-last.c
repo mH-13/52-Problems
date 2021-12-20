384974941f0bd89c56a2fa93d5f8f2762f37e387
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-12-20 23:40:23 +0600

    Update 6. sum-of-1st-last.c

--- 6. sum-of-1st-last.c
+++ 6. sum-of-1st-last.c
@@ -1,1 +1,19 @@
-#incl
+#include <stdio.h>
+int main(){
+    
+    int i,T,N, sum;
+    scanf("%d", &T);
+    
+    for(i=1; i<=T; i++){
+        scanf("%d", &N);
+        int lsd= N%10;
+        int msd = N;
+        while(N>9){
+            N = N/10;
+        }
+        msd = N;
+        int sum = lsd+ msd;
+        printf("Sum = %d\n", sum);
+    }
+    return 0;
+}
