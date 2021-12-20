154533c2e0f5aef38efeefc8ebd956aa1ad4e7ec
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-12-21 01:29:43 +0600

    Update 7. How-many-number.c

--- 7. How-many-number.c
+++ 7. How-many-number.c
@@ -1,1 +1,32 @@
-s
+//strtol() -- string to long converter 
+//-- long int strtol (const char* str, char** endptr, int base);
+//stdlib.h -- header file of strtol()
+//atol -- Convert string to long integer (function )
+//strtoul -- Convert string to unsigned long integer (function )
+// strtod -- Convert string to double (function )
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i,T;
+    scanf("%d",&T);
+    for(i=1; i<=T; i++){
+        char line[1000000];
+        scanf(" %[^\n]", line);
+        char *p, *e;
+        long input;
+        int count =0;
+        p = line;
+        for(p=line; ; p=e){
+            input = strtol(p, &e, 10);
+            if(p == e){
+                break;
+            }
+            count++;
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
